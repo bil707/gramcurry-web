58eb575b2052c7423d84a9cd0559590704330e05
bil707 <141075839+bil707@users.noreply.github.com>
2024-10-06 22:38:13 +0800

    commit

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.155.0/build/three.module.js';
-//import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.155.0/examples/jsm/controls/OrbitControls.js';
+//import * as THREE from 'three';
 
 // Scene setup
 const scene = new THREE.Scene();
